920ade6bfa7b8c14f93ddb180da878c456d222f2
Artem Karmaz <inste@icloud.com>
2019-01-15 14:59:54 +0200

    Initial Commit

---
+++ WhatWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatWeather
+//
+//  Created by Artem Karmaz on 1/15/19.
+//  Copyright © 2019 Johansson Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
